2c6a14a503692696f3c7ebfe71fa6154189887ae
Navneet0801 <nk9810328872@gmail.com>
2022-08-19 17:33:19 +0530

    Script added and player configuration completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,21 @@
+let editedPlayer = 0;
+
+const players = [
+    {
+        name: '',
+        symbol: 'X'
+    },
+    {
+        name: '',
+        symbol: '0'
+    }
+];
+
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backDropElement = document.querySelector(".back-drop");
+const formElement = document.getElementById('form');
+const errorOutputElement = document.getElementById('config-error');
+
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
@@ -12,3 +28,5 @@ editPlayer2BtnElement.addEventListener('click', openConfigPlayer);
 //closes the player configuration window
 cancelConfigBtnElement.addEventListener('click', closeConfigPlayer);
 backDropElement.addEventListener('click', closeConfigPlayer);
+
+formElement.addEventListener('submit', savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,5 @@
-function openConfigPlayer(){
+function openConfigPlayer(event){
+    editedPlayer = +event.target.dataset.playerid;  //gives 1 or 2
     playerConfigOverlayElement.style.display = 'block';
     backDropElement.style.display = 'block';
 }
@@ -6,4 +7,27 @@ function openConfigPlayer(){
 function closeConfigPlayer(){
     playerConfigOverlayElement.style.display = 'none';
     backDropElement.style.display = 'none';   
+    formElement.firstElementChild.classList.remove('error');
+    errorOutputElement.textContent = "";
+    const resetplayer = document.getElementById('playername');
+    resetplayer.value = "";
+}
+
+function savePlayerConfig(event){
+    event.preventDefault();
+    const formData = new FormData(form);  //event.target inplace for form also works
+    const enteredPlayerName = formData.get('playername').trim();  //it takes the input and trim it  trim -> "   Navneet Kumar   " = "Navneet Kumar"
+
+    if(!enteredPlayerName){
+        form.firstElementChild.classList.add('error');
+        errorOutputElement.textContent = "Please enter a valid Name!";
+        return;
+    }
+
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+    players[editedPlayer - 1].name = enteredPlayerName;
+
+    closeConfigPlayer();
 }
